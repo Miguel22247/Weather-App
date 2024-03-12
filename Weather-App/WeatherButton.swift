cb6f9b274e8d8c8131f9f58058738589de652759
Miguel Pacheco <minipachru@gmail.com>
2024-03-12 20:32:29 -0300

    Initial Commit

---
+++ Weather-App/WeatherButton.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherButton.swift
+//  Weather-App
+//
+//  Created by Miguel Pacheco on 13/3/24.
+//
+
+import Foundation
---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Miguel Pacheco on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
