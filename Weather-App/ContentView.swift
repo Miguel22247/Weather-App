1aeebff0c6f7f54557832a1d6166fd11e35c2859
Miguel Pacheco <minipachru@gmail.com>
2024-03-13 20:24:14 -0300

    Add new Swift file

--- Weather-App/ContentView.swift
+++ Weather-App/ContentView.swift
@@ -41,28 +41,6 @@ struct ContentView: View {
     ContentView()
 }
 
-struct WeatherDayView: View {
-    var dayOfWeek: String
-    var imageName: String
-    var temperature: Int
-
-    var body: some View {
-        VStack{
-            Text(dayOfWeek)
-                .font(.system(size: 16, weight: .medium, design: .default))
-                .foregroundColor(.white)
-            Image(systemName: imageName)
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 40)
-            Text("\(temperature)°")
-                .font(.system(size: 28, weight: .medium))
-                .foregroundColor(.white)
-        }
-    }
-}
-
 struct BackgroundView: View {
     @Binding var isNight: Bool
 
---
+++ Weather-App/WeatherDayView.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherDayView.swift
+//  Weather-App
+//
+//  Created by Miguel Pacheco on 13/3/24.
+//
+
+import SwiftUI
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
